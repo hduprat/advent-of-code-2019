63647c3b402377fa138fb7fdf093e97513552e4b
hduprat <hugod@bam.tech>
2020-12-25 11:42:44 +0100

    :sparkles: (day 1) part two

--- 1.ts
+++ 1.ts
@@ -3,12 +3,26 @@ import { getLinesOfFile } from "./utils/getLinesOfFile";
 
 const getModuleFuelRequirement = (
   mass: number,
+  countFuel: boolean,
   verbose: boolean = false
 ): number => {
   const requirement = Math.floor(mass / 3) - 2;
   if (verbose)
     text("The fuel requirement for a module of mass", mass, "is", requirement);
-  return requirement;
+  if (!countFuel) return Math.max(requirement, 0);
+  if (requirement <= 0) return 0;
+
+  const additionalRequirement =
+    requirement + getModuleFuelRequirement(requirement, true);
+  if (verbose)
+    text(
+      "The fuel requirement for a mass",
+      requirement,
+      "of fuel is",
+      additionalRequirement,
+      "(including fuel itself)."
+    );
+  return additionalRequirement;
 };
 
 const playScenario = async (path: string) => {
@@ -28,12 +42,19 @@ const playScenario = async (path: string) => {
   result("result:", totalFuelRequirement);
   lineBreak();
 
-  // title(`Second exercise: ZZZZ.`, "green");
+  title(
+    `Second exercise: Calculate the fuel requirements for the rocket INCLUDING FUEL MASS.`,
+    "green"
+  );
 
-  // // code here
+  const totalFuelRequirementCountingFuel = lines.reduce(
+    (requirement, module) =>
+      requirement + getModuleFuelRequirement(parseInt(module), true, true),
+    0
+  );
 
-  // result("result:", 0);
-  // lineBreak();
+  result("result:", totalFuelRequirementCountingFuel);
+  lineBreak();
 };
 
 async function main() {
