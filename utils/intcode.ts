9f0bf8d957f1b3adf58c3f1ed257f2460184e5fe
hduprat <hugod@bam.tech>
2020-12-25 12:20:57 +0100

    :recycle: (intcode) prepare for extension

--- utils/intcode.ts
+++ utils/intcode.ts
@@ -1,18 +1,27 @@
+import { text } from "./console";
+
 export const getProgram = (line: string): number[] =>
   line.split(",").map((val) => parseInt(val));
 
 export const executeProgram = (program: number[]) => {
-  for (let i = 0; i < program.length; i += 4) {
+  let i = 0;
+  while (true) {
     const opcode = program[i];
     switch (opcode) {
-      case 1:
-        program[program[i + 3]] =
-          program[program[i + 1]] + program[program[i + 2]];
+      case 1: {
+        const parameters = program.slice(i, i + 4);
+        program[parameters[3]] =
+          program[parameters[1]] + program[parameters[2]];
+        i += 4;
         continue;
-      case 2:
-        program[program[i + 3]] =
-          program[program[i + 1]] * program[program[i + 2]];
+      }
+      case 2: {
+        const parameters = program.slice(i, i + 4);
+        program[parameters[3]] =
+          program[parameters[1]] * program[parameters[2]];
+        i += 4;
         continue;
+      }
       case 99:
         return;
       default:
